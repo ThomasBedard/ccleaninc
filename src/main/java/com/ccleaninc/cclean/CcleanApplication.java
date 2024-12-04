523ee6cbbed0544006f84c3eb40bc5f97d64ddb0
Thomas <thomas.bedard0@gmail.com>
2024-12-04 15:25:54 -0500

    Initial commit

---
+++ src/main/java/com/ccleaninc/cclean/CcleanApplication.java
@@ -0,0 +1,13 @@
+package com.ccleaninc.cclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccleaninc/cclean/CcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccleaninc.cclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
