3b82b2487d802ed235b33b3f663572bee8731121
Drexjar <jarredx@bell.net>
2024-12-06 23:04:58 -0500

    feat: Create login page and updated project files

---
+++ cclean-frontend/src/pages/Appointments.tsx
@@ -0,0 +1,9 @@
+const Appointments = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Appointments Page</h1>
+      </div>
+    );
+  };
+
+  export default Appointments ;
