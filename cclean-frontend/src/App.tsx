386996b519910ceaf30e37040c1082ceb9652bdc
Drexjar <jarredx@bell.net>
2024-12-08 14:34:34 -0500

    Resolve merge conflicts

--- cclean-frontend/src/App.tsx
+++ cclean-frontend/src/App.tsx
@@ -10,27 +10,9 @@ import Register from './pages/Register';
 import Schedule from './pages/Schedule'; // Import the Schedule page
 
 
-
 const App = () => {
   return (
     <Router>
-      {/* Shared background color and full height */}
-      <div className="bg-gradient-to-b from-[#E4EDF5] to-[#F7F8FC] min-h-screen">
-        <Navbar />
-        <div className="max-w-7xl mx-auto px-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/appointments" element={<Appointments />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/schedule" element={<Schedule />} /> {/* Add this line */}
-          </Routes>
-        </div>
-      </div>
-
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -40,6 +22,8 @@ const App = () => {
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/schedule" element={<Schedule />} /> {/* Add this line */}
+
         </Routes>
       </Layout>
     </Router>
@@ -47,3 +31,4 @@ const App = () => {
 };
 
 export default App;
+
