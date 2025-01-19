d9a16b02944b813562535ed93f39e0605b947ec5
Thomas <thomas.bedard0@gmail.com>
2025-01-19 00:55:34 -0500

    1

--- src/main/java/com/ccleaninc/cclean/appointmentssubdomain/businesslayer/AppointmentService.java
+++ src/main/java/com/ccleaninc/cclean/appointmentssubdomain/businesslayer/AppointmentService.java
@@ -8,14 +8,8 @@ import java.util.List;
 public interface AppointmentService {
 
     List<AppointmentResponseModel> getAllAppointments();
-<<<<<<< HEAD
     AppointmentResponseModel createAppointment(AppointmentRequestModel requestModel);
-<<<<<<< HEAD
-}
-=======
-=======
 
->>>>>>> 30f5822 (fix(CCICC-68): Fixed appointment creation to correctly use customer ID and updated unit tests)
     AppointmentResponseModel getAppointmentByAppointmentId(String appointmentId);
 
     AppointmentResponseModel addAppointment(AppointmentRequestModel appointmentRequestModel);
@@ -25,4 +19,4 @@ public interface AppointmentService {
     void deleteAppointmentByAppointmentId(String appointmentId);
 
 }
->>>>>>> 93ff3db (Implemented the CRUD operations for Appointments)
+
