63a50ba68c20e614dfc6f0ec6d4005f79e6dfec2
Thomas <thomas.bedard0@gmail.com>
2025-02-02 21:41:20 -0500

    fix application yml

--- src/main/java/com/ccleaninc/cclean/feedbacksubdomain/presentationlayer/FeedbackController.java
+++ src/main/java/com/ccleaninc/cclean/feedbacksubdomain/presentationlayer/FeedbackController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("api/v1")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://ccleaninc.vercel.app"
+})
 public class FeedbackController {
     private final FeedbackService feedbackService;
 
